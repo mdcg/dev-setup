c52a3ae0be32cc83d7dc23c5594ee9e665e45843
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-01-04 10:29:58 -0300

    Inicializando dotfiles maravilhosos

---
+++ install/generic_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+# Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update -y
+sudo apt-get install google-chrome-stable
+
+# Pyenv
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
+pyenv install 3.6.5
+pyenv global 3.6.5
+
+# Docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
