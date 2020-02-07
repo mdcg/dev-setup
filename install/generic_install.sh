f9bc5450dda47cffd9a938f11672fad082fc9ac7
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-02-07 15:49:53 -0300

    Adding spotify install

--- install/generic_install.sh
+++ install/generic_install.sh
@@ -36,3 +36,9 @@ sudo docker run hello-world
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Spotify
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update -y && sudo apt-get install -y spotify-client
