40e34f1824b93d222af7a5d9c8003e94ffddfd8c
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-07-19 01:58:19 -0300

    Fixing visual-studio-code-install.sh script

--- ubuntu/development/visual-studio-code-install.sh
+++ ubuntu/development/visual-studio-code-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install software-properties-common apt-transport-https wget -y
 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
